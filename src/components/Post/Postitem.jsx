47ef2508deca3fa1a2c7f3beb1d109aaed71b4fb
ABHIRAM KRISHNA S <abhiramkrishna085@gmail.com>
2023-02-26 19:02:28 +0530

    Modified the stories.jsx component

--- src/components/Post/Postitem.jsx
+++ src/components/Post/Postitem.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { AiFillHeart } from "react-icons/ai";
 import { FaRegComment, FaBookmark } from "react-icons/fa";
 import { GrShareOption } from "react-icons/gr";
+import Comments from './Comments';
 
 const Postitem = (props) => {
     const [like, setLike] = useState(false);
@@ -14,8 +15,6 @@ const Postitem = (props) => {
 
     const { post } = props;
     const { userid, datetime, description, images, location,userImage } = post;
-
-    
 
     useEffect(()=>{
         const dataNew = new Uint8Array(userImage.data.data);
@@ -75,52 +74,7 @@ const Postitem = (props) => {
 
 
             <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog  my-3 modal-xl">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <div className="comment-modal-title">
-                                <h1 className="modal-title fs-5" id="exampleModalLabel">Image</h1>
-                                <h1 className="modal-title fs-5" id="exampleModalLabel2">Comments</h1>
-                            </div>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body d-flex justify-content-between">
-                            <img className='comment-image' src={postImage} alt="" onDoubleClick={() => { setLike(true) }} />
-                            <div className='comment-section-main'>
-                                <div className="border-bottom py-2 d-flex m-2">
-                                    <i className='activity-icon-bg'>
-                                        <img className='profile-pic' src={postUserImage} alt="" />
-                                    </i>
-                                    <div className='mx-0'>
-                                        <p style={{ fontWeight: "700", fontSize: "14px", paddingLeft: "10px", height: "8px" }}>{userid}</p>
-                                        <p style={{ fontWeight: "600", fontSize: "11px", paddingLeft: "10px", color: "#818589", width: "150px", height: "1px" }}>{location} </p>
-                                    </div>
-                                </div>
-                                <div className="comment-section border-bottom">
-                                    <div className='commented-user d-flex m-2'>
-                                        <i className='activity-icon-bg'>
-                                            <img className='profile-pic' src={postUserImage} alt="" />
-                                        </i>
-                                        <div className='my-0'>
-                                            <p style={{ fontWeight: "700", fontSize: "14px", paddingLeft: "10px", height: "8px" }}>Irfan Rasheed KC</p><p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit quasi nulla, error sapiente quos blanditiis mollitia fugiat iusto ipsum eligendi iste dolores perspiciatis cum laborum unde quas molestias. Neque, non.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="like-comment-save">
-                                    <AiFillHeart onClick={handleLike} className={like ? "liked" : "like-icon"} />
-                                    <i type="button">
-                                        <FaRegComment className='search-icon mx-4' />
-                                    </i>
-                                    <GrShareOption className='search-icon' />
-                                    <FaBookmark onClick={handleSave} className={save ? "save-icon-clicked" : "save-icon"} />
-                                </div>
-                                <div className="add-comment">
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Comments post={post}/>
             </div>
 
             <div className="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
--- src/components/Stories&Suggestion/Stories.jsx
+++ src/components/Stories&Suggestion/Stories.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import "./Stories.css";
+import DemoContext from '../../contexts/Democontext';
 const Stories = () => {
   const a = [];
   for (let i = 0; i < 20; i++) {
     a.push(i);
   }
+
+  const context = useContext(DemoContext);
+  const { imageData, setImageData } = context;
+
+  useEffect(() => {
+    const StoredImage = localStorage.getItem('image');
+    const ImageType = localStorage.getItem('imgtype');
+    setImageData(`data:${ImageType};base64,` + StoredImage)
+  }, [])
+
   return (
     <div className='bg-light story-div'>
       <div className="story-list">
+        <div className="story-item">
+          <img className='story-pic mx-2 my-3' src={imageData} alt="" />
+          <p>Your Story</p>
+        </div>
         {a.map(() => <div className="story-item">
-          <img className='story-pic mx-2 my-3' src="https://w7.pngwing.com/pngs/326/85/png-transparent-google-logo-google-text-trademark-logo.png" alt="" />
+          <img className='story-pic mx-2 my-3' src="https://cdn.imgbin.com/8/20/20/imgbin-samsung-galaxy-a8-a8-user-login-telephone-avatar-pawn-pvE7Qhr6Zk7kLJpGiWZ9FFRVf.jpg" alt="" />
           <p>abhiram</p>
         </div>)}
 
@@ -17,5 +32,5 @@ const Stories = () => {
     </div>
   )
 }
-
+// 
 export default Stories
