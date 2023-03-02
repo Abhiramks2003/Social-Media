45cccef39624461ef2d9ebcdbf4c2b2053ba30eb
ABHIRAM KRISHNA S <abhiramkrishna085@gmail.com>
2023-03-02 19:37:24 +0530

    User can now view their post in the profile page

---
+++ src/components/Profile/MyPosts.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import { TabPanel } from 'react-tabs';
+import "./Profile.css"
+import 'react-tabs/style/react-tabs.css';
+const MyPosts = (props) => {
+    const { post } = props;
+    const { datetime, description, images, location } = post;
+    const [postImage, setPostImage] = useState(null);
+    let dateObj = new Date(datetime);
+    var day = dateObj.getDate();
+    var month = dateObj.toLocaleString('default', { month: 'long' })
+    var year = dateObj.getFullYear();
+    var date = (day.toString() + " " + month + " " + year.toString());
+
+    useEffect(() => {
+        const imageData = images?.[0];
+        if (imageData) {
+            const { data, mimetype } = imageData;
+            const newPostImage = `data:${mimetype};base64,${data}`;
+            setPostImage(newPostImage);
+        }
+    }, [])
+
+    return (
+        <>
+            <img src={postImage} alt="aaa" />
+        </>
+
+    )
+}
+
+export default MyPosts
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,20 +1,38 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import axios from 'axios';
 import "./Profile.css";
 import Profilebar from './Profilebar';
 import DemoContext from '../../contexts/Democontext';
+import MyPosts from './MyPosts';
 
 const Profile = () => {
   const context = useContext(DemoContext);
   const { name, setName, imageData, setImageData } = context;
-  
+  const host = 'http://192.168.1.53:5000';
+  const userId = localStorage.getItem('userId');
+  let url = `${host}/api/profile/${userId}`;
+  const [myPosts, setMyPosts] = useState([]);
+
+  const LoadmyPosts = async () => {
+    try {
+      const res = await axios.get(url);
+      console.log(res.data);
+      setMyPosts(res.data);
+    } catch (error) {
+
+    }
+  }
+
+
   useEffect(() => {
     const Storedname = localStorage.getItem('name');
     const StoredImage = localStorage.getItem('image');
     const ImageType = localStorage.getItem('imgtype');
-    setName(Storedname)
-    setImageData(`data:${ImageType};base64,` + StoredImage)
+    setName(Storedname);
+    setImageData(`data:${ImageType};base64,` + StoredImage);
+    LoadmyPosts();
   }, [])
 
   return (
@@ -27,7 +45,7 @@ const Profile = () => {
           <h4>{name}</h4>
           <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h6>
           <div className='publicity'>
-            <h5>Posts  17</h5>
+            <h5>Posts  {myPosts.length}</h5>
             <h5>Followers 10k</h5>
             <h5>Following 620</h5>
           </div>
@@ -41,12 +59,16 @@ const Profile = () => {
               <Tab>Liked</Tab>
             </div>
           </TabList>
-
           <TabPanel>
             <div className='my-posts'>
-              <img type='button'  src="https://ihitthebutton.com/wp-content/uploads/2016/04/nyc-instagram-spots-top-of-the-rock-sunset-1-scaled.jpg" alt="" />
+              {myPosts.map((post) => <>
+                <MyPosts post={post} />
+              </>
+              )}
             </div>
           </TabPanel>
+
+
           <TabPanel>
             <div className='my-videos'>
 
@@ -65,7 +87,7 @@ const Profile = () => {
             </div>
           </TabPanel>
         </Tabs>
-      
+
       </div>
     </>
 
